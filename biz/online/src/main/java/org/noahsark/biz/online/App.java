279acd3aabd5f2a4cb988cf88a08b88b845cfaf7
noahsarkzhang-ts <chuanzhang_ts@163.com>
2021-05-09 21:11:45 +0800

    提交业务模块

--- biz/online/src/main/java/org/noahsark/biz/online/App.java
+++ biz/online/src/main/java/org/noahsark/biz/online/App.java
@@ -1,10 +1,10 @@
-package org.noahsark.gw.ws;
+package org.noahsark.biz.online;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App 
+public class App
 {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
---
+++ biz/online/src/main/java/org/noahsark/biz/online/listener/ApplicationStopListener.java
@@ -0,0 +1,24 @@
+package org.noahsark.biz.online.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: noahsark
+ * @version:
+ * @date: 2020/3/13
+ */
+@Component
+public class ApplicationStopListener implements ApplicationListener<ContextClosedEvent> {
+
+    private static Logger logger = LoggerFactory.getLogger(ApplicationStopListener.class);
+
+
+    @Override
+    public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
+        logger.info("Application stoped!");
+    }
+}
